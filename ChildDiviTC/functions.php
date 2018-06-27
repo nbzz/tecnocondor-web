7b4bc2bc2b5ca9cc9b2a36fb91e97aec82c90ae0
nobodyzz <harold.nb@gmail.com>
2018-06-26 20:55:30 -0400

    modificacion descripción

--- ChildDiviTC/functions.php
+++ ChildDiviTC/functions.php
@@ -1,7 +1,19 @@
 <?php
+/**
+ * Divi Tecnocondor Child Theme
+ * Functions.php
+ *
+ * ===== NOTAS ==================================================================
+ * A diferencia de style.css, las functions.php de un tema hijo no anulan 
+ * a su homólogo del padre. En su lugar, se carga además de las funciones
+ * del padre functions.php. 
+ * De este modo, functions.php de un tema hijo proporciona un método inteligente 
+ * y sin problemas para modificar la funcionalidad de un tema padre. 
+ * 
+ * 
+ * =============================================================================== */
+ 
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
-//add
